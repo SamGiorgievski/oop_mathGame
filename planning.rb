4297507f60b5a7f94e0cf392f15a39920ba2f97a
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-23 14:46:11 -0500

    plan game

---
+++ planning.rb
@@ -0,0 +1,18 @@
+# Nouns: Players, Score, Scoreboard, Questions, Answers, Response, Turn/Round, Winner 
+
+
+# Classes:
+# 1: player_info - player_id, current_score
+# Initialize - player_id, current_score
+# Public: player_id, current_score
+
+# 2: game_master - scoreboard, round(new turn or game over)
+# Initialize - scoreboard, round
+# Public: scoreboard
+
+# 3. question_master - questions, answers, correct_quote, incorrect_quote, input
+# Initialize - questions, answers
+# public - questions, answers, input
+
+
+# Note: Can't be pre-written questions
